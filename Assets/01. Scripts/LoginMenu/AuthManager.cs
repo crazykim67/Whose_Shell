8bde640c32ff44c2b0e2098d99fb3fc4c5118543
crazykim67 <crazykim67@gmail.com>
2023-11-01 17:32:22 +0900

    파이어 베이스 로그인 시스템 구현

---
+++ Assets/01. Scripts/LoginMenu/AuthManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Firebase.Auth;
+using System;
+
+public class AuthManager : MonoBehaviour
+{
+    #region Instance
+
+    private static AuthManager instance;
+
+    public static AuthManager Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = new AuthManager();
+                return instance;
+            }
+
+            return instance;
+        }
+    }
+
+    #endregion 
+
+    private Firebase.Auth.FirebaseAuth auth;
+
+    public string currentId;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+            Destroy(this.gameObject);
+
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+    }
+
+    // 로그인 메서드
+    public void OnLogin(string _id, string _email, string _pass)
+    {
+        auth.SignInWithEmailAndPasswordAsync($"{_id}@{_email}", _pass).ContinueWith(
+            task =>
+            {
+                if (task.IsCompleted && !task.IsFaulted && !task.IsCanceled)
+                { 
+                    Debug.Log($"{_id} is Login Successful");
+                    currentId = _id;
+                }
+                else if (task.IsFaulted)
+                {
+                    Debug.Log("Login is Failed...!");
+                    return;
+                }
+            });
+    }
+
+    // 회원가입 메서드
+    public void OnRegister(string _id, string _pass)
+    {
+        auth.CreateUserWithEmailAndPasswordAsync($"{_id}", _pass).ContinueWith(
+            task =>
+            {
+                if (task.IsCompleted && !task.IsFaulted && !task.IsCanceled)
+                {
+                    Debug.Log($"{_id} is Register Successful");
+                }
+                else if (task.IsFaulted)
+                {
+                    Debug.Log("Register is Failed...!");
+                    return;
+                }
+            });
+    }
+
+    // 로그아웃 메서드
+    public void OnLogOut()
+    {
+        if(auth.CurrentUser != null)
+            auth.SignOut();
+    }
+
+    public void OnApplicationQuit() => OnLogOut();
+}
---
+++ Assets/01. Scripts/LoginMenu/LoginMenuUI.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LoginMenuUI : MonoBehaviour
+{
+
+    [Header("Login Menu")]
+    public GameObject loginMenu;
+    public TMP_InputField lg_IdInputField;
+    public TMP_InputField lg_PassInputField;
+    public TMP_Dropdown lg_dropdown;
+    public Button lg_loginBtn;
+    public Button lg_registerBtn;
+    public Button exitBtn;
+
+    [Header("Register Menu")]
+    public GameObject registerMenu;
+    public TMP_InputField rg_IdInputField;
+    public TMP_InputField rg_PassInputField;
+    public TMP_InputField rg_PassCheckInputField;
+    public TMP_Dropdown rg_dropdown;
+    public Button rg_registerBtn;
+    public Button rg_backBtn;
+
+    private void Awake()
+    {
+        lg_loginBtn.onClick.AddListener(OnLogin);
+        lg_registerBtn.onClick.AddListener(OnRegisterMenu);
+        rg_backBtn.onClick.AddListener(OnLoginMenu);
+        rg_registerBtn.onClick.AddListener(OnRegist);
+    }
+
+    // 로그인 버튼
+    public void OnLogin()
+    {
+        if (lg_IdInputField.Equals(string.Empty))
+        {
+            Debug.Log("ID Input Field is Empty...!");
+            return;
+        }
+        else if (lg_IdInputField.text.Equals(string.Empty))
+        {
+            Debug.Log("Password Input Field is Empty...!");
+            return;
+        }
+
+        // 파이어베이스 로그인
+        AuthManager.Instance.OnLogin(lg_IdInputField.text, lg_dropdown.options[lg_dropdown.value].text, lg_PassInputField.text);
+    }
+
+    // 회원가입 완료 버튼
+    public void OnRegist()
+    {
+        // 아이디 입력칸 비어있음
+        if (rg_IdInputField.text.Equals(string.Empty))
+        {
+            Debug.Log("ID Input Field is Empty...!");
+            return;
+        }
+        // 아이디 입력 값 8자 미만 15자 초과했음
+        else if (rg_IdInputField.text.Length < 8 || rg_IdInputField.text.Length > 15)
+        {
+            Debug.Log("ID Must at least 8 char and at most 15 char...!");
+            return;
+        }
+        // 비밀번호 입력칸 비어있음
+        else if (rg_PassInputField.Equals(string.Empty))
+        {
+            Debug.Log("Password Input Field is Empty...!");
+            return;
+        }
+        // 비밀번호 입력 값 8자 미만 15자 초과했음
+        else if (rg_PassInputField.text.Length < 8 || rg_PassInputField.text.Length > 15)
+        {
+            Debug.Log("Password Must at least 8 char and at most 15 char...!");
+            return;
+        }
+        // 비밀번호 확인 입력칸 비어있음
+        else if (rg_PassCheckInputField.text.Equals(string.Empty))
+        {
+            Debug.Log("Password Check Input Field is Empty...!");
+            return;
+        }
+        // 비밀번호 확인 입력 값이 비밀번호 입력 값과 일치하지 않음
+        else if (!rg_PassCheckInputField.text.Equals(rg_PassInputField.text))
+        {
+            Debug.Log("Passsword Input Field is Different from PasswordCheck Input Field");
+            return;
+        }
+
+        AuthManager.Instance.OnRegister($"{rg_IdInputField.text}@{rg_dropdown.options[rg_dropdown.value].text}", rg_PassInputField.text);
+        OnLoginMenu();
+    }
+
+    // 로그인화면 세팅
+    public void OnLoginMenu()
+    {
+        loginMenu.SetActive(true);
+        lg_IdInputField.text = "";
+        lg_PassInputField.text = "";
+        lg_dropdown.value = 0;
+
+        registerMenu.SetActive(false);
+    }
+
+    // 회원가입화면 세팅
+    public void OnRegisterMenu()
+    {
+        loginMenu.SetActive(false);
+
+        registerMenu.SetActive(true);
+        rg_IdInputField.text = "";
+        rg_PassInputField.text = "";
+        rg_PassCheckInputField.text = "";
+        rg_dropdown.value = 0;
+    }
+}
